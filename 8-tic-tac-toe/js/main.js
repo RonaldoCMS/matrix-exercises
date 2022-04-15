bbe4432d40ec0ea3d784f797789baf06b06b7d89
RonaldoCMS <ronaldocms@outlook.it>
2022-04-15 10:55:43 +0200

    IA tic-tac-toe

--- 8-tic-tac-toe/js/main.js
+++ 8-tic-tac-toe/js/main.js
@@ -1,56 +1,68 @@
 "use strict";
 let im = "X";
-let punteggioX = 0;
-let punteggioO = 0;
-function changeIm() {
-    let consiglio = document.getElementById("consiglio1");
-    if (im == "X") {
-        im = "O";
-        consiglio.innerHTML = "E' il turno di... O";
-    }
-    else {
-        im = "X";
-        consiglio.innerHTML = "E' il turno di... X";
-    }
-}
 let fields = document.getElementsByClassName("field");
+let consiglio = document.getElementById("advice1");
+let computer = false;
+let scoreX = 0;
+let scoreO = 0;
 function handleOnClickField(id) {
-    document.getElementById(id).value = im;
-    document.getElementById(id).disabled = true;
-    isTris();
-    changeIm();
+    let input = document.getElementById(id);
+    input.value = im;
+    input.disabled = true;
+    _isTris();
+    if (computer) {
+        _changePlayer();
+        let computerInput = Math.floor(Math.random() * 9 + 1);
+        while (_isAlreadyClick("f" + computerInput)) {
+            computerInput = Math.floor(Math.random() * 9 + 1);
+            console.log("STO CERCANDO NUOVO VALORE..." + computerInput + 1);
+        }
+        console.log("FATTO" + computerInput);
+        document.getElementById("f" + computerInput).value = "O";
+        _isTris();
+    }
+    _changePlayer();
 }
-function isTris() {
-    if (watchTris2Option(0, 1, 2) || watchTris2Option(3, 4, 5) ||
-        watchTris2Option(6, 7, 8) || watchTris2Option(0, 3, 6) ||
-        watchTris2Option(1, 4, 7) || watchTris2Option(2, 5, 8) ||
-        watchTris2Option(0, 4, 8) || watchTris2Option(2, 4, 6)) {
+function handlePlayWithComputer() {
+    computer = true;
+    handlePlay();
+}
+function handlePlay() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("play").style.display = "block";
+}
+function _isAlreadyClick(id) {
+    let field = document.getElementById(id);
+    if (field.value == "") {
+        return false;
+    }
+    return true;
+}
+function _isTris() {
+    if (_detectGenericWinner(0, 1, 2) || _detectGenericWinner(3, 4, 5) ||
+        _detectGenericWinner(6, 7, 8) || _detectGenericWinner(0, 3, 6) ||
+        _detectGenericWinner(1, 4, 7) || _detectGenericWinner(2, 5, 8) ||
+        _detectGenericWinner(0, 4, 8) || _detectGenericWinner(2, 4, 6)) {
         let punteggio = document.getElementById("punteggio" + im);
         if (im == "X") {
-            punteggio.innerHTML = "X = " + ++punteggioX;
+            punteggio.innerHTML = "X = " + ++scoreX;
         }
         else {
-            punteggio.innerHTML = "O = " + ++punteggioO;
+            punteggio.innerHTML = "O = " + ++scoreO;
         }
-        reset();
+        _reset();
     }
-    else if (isDraw()) {
-        reset();
+    else if (_isDraw()) {
+        _reset();
     }
 }
-function watchTris2Option(case1, case2, case3) {
-    return watchTris(case1, case2, case3, "X") || watchTris(case1, case2, case3, "O");
-}
-function watchTris(case1, case2, case3, type) {
-    return fields[case1].value == type && fields[case2].value == type && fields[case3].value == type;
-}
-function reset() {
+function _reset() {
     for (let i = 0; i < fields.length; i++) {
         fields[i].value = "";
         fields[i].disabled = false;
     }
 }
-function isDraw() {
+function _isDraw() {
     let counter = 0;
     for (let i = 0; i < fields.length; i++) {
         if (fields[i].value != "")
@@ -60,4 +72,20 @@ function isDraw() {
         return true;
     }
     return false;
+}
+function _detectGenericWinner(field1, field2, field3) {
+    return _detectWinner(field1, field2, field3, "X") || _detectWinner(field1, field2, field3, "O");
+}
+function _detectWinner(field1, field2, field3, type) {
+    return fields[field1].value == type && fields[field2].value == type && fields[field3].value == type;
+}
+function _changePlayer() {
+    if (im == "X") {
+        im = "O";
+        consiglio.innerHTML = "E' il turno di... O";
+    }
+    else {
+        im = "X";
+        consiglio.innerHTML = "E' il turno di... X";
+    }
 }
--- 8-tic-tac-toe/ts/main.ts
+++ 8-tic-tac-toe/ts/main.ts
@@ -1,66 +1,79 @@
 let im = "X";
-
-let punteggioX = 0;
-let punteggioO = 0;
-
-
-function changeIm() {
-    let consiglio = (document.getElementById("consiglio1") as HTMLParagraphElement);
-    if(im == "X") {
-        im = "O";
-        consiglio.innerHTML = "E' il turno di... O";
-    }  else  {
-        im = "X";
-        consiglio.innerHTML = "E' il turno di... X";
-    }
-}
-
-let fields = (document.getElementsByClassName("field") as HTMLCollectionOf<HTMLInputElement>)
+let fields = (document.getElementsByClassName("field") as HTMLCollectionOf<HTMLInputElement>);
+let consiglio = (document.getElementById("advice1") as HTMLParagraphElement);
+let computer = false;
+let scoreX = 0;
+let scoreO = 0;
 
 
 function handleOnClickField(id : string) : void {
-    (document.getElementById(id) as HTMLInputElement).value = im;
-    (document.getElementById(id) as HTMLInputElement).disabled = true;
-    isTris();
-    changeIm();
+    let input = (document.getElementById(id) as HTMLInputElement);
+    input.value = im;
+    input.disabled = true;
+    _isTris();
+    if(computer) {
+        _changePlayer();
+        let computerInput = Math.floor(Math.random() * 9 + 1);
+        while(_isAlreadyClick("f"+computerInput)) {
+            computerInput = Math.floor(Math.random() * 9 + 1);
+            console.log("STO CERCANDO NUOVO VALORE..." + computerInput+1);
+        }
+        console.log("FATTO" + computerInput);
+        (document.getElementById("f"+computerInput) as HTMLInputElement).value="O";
+        _isTris();
+    }
+    _changePlayer();
+}
+
+function handlePlayWithComputer() {
+    
+    computer = true;
+}
+
+function handlePlay(): void {
+    (document.getElementById("start") as HTMLDivElement).style.display = "none";
+    (document.getElementById("play") as HTMLDivElement).style.display = "block";
+
 }
 
 
-function isTris() : void {
-    if(watchTris2Option(0,1,2) || watchTris2Option(3,4,5) ||
-        watchTris2Option(6,7,8) || watchTris2Option(0,3,6) ||
-        watchTris2Option(1,4,7) || watchTris2Option(2,5,8) ||
-        watchTris2Option(0,4,8) || watchTris2Option(2,4,6)) {  
+
+function _isAlreadyClick(id: string) : boolean {
+    let field = document.getElementById(id) as HTMLInputElement;
+    if(field.value == "") {
+        return false;
+    }
+    return true;
+}
+
+
+function _isTris() : void {
+    if(_detectGenericWinner(0,1,2) || _detectGenericWinner(3,4,5) ||
+        _detectGenericWinner(6,7,8) || _detectGenericWinner(0,3,6) ||
+        _detectGenericWinner(1,4,7) || _detectGenericWinner(2,5,8) ||
+        _detectGenericWinner(0,4,8) || _detectGenericWinner(2,4,6)) {  
         let punteggio = (document.getElementById("punteggio"+im) as HTMLParagraphElement);
         if(im == "X") {
-            punteggio.innerHTML = "X = " + ++punteggioX;
+            punteggio.innerHTML = "X = " + ++scoreX;
         }
         else {            
-            punteggio.innerHTML = "O = " + ++punteggioO;
+            punteggio.innerHTML = "O = " + ++scoreO;
         }
-        reset();
-    } else if(isDraw()) {
-        reset();
+        _reset();
+    } else if(_isDraw()) {
+        _reset();
     }
 }
 
-function watchTris2Option(case1: number, case2: number, case3: number) {
-    return  watchTris(case1, case2, case3, "X") || watchTris(case1, case2, case3, "O");
-} 
 
-function watchTris(case1: number, case2: number, case3: number, type: String) {
-    return fields[case1].value == type && fields[case2].value == type && fields[case3].value == type;
-}
-
-
-function reset() : void {
+function _reset() : void {
     for(let i = 0; i<fields.length; i++) {
         fields[i].value = "";
         fields[i].disabled = false;
     }
 }
 
-function isDraw() : boolean {
+function _isDraw() : boolean {
     let counter = 0;
     for(let i = 0; i<fields.length; i++) {
         if(fields[i].value != "") 
@@ -73,6 +86,23 @@ function isDraw() : boolean {
     return false;
 }
 
+function _detectGenericWinner(field1: number, field2: number, field3: number) {
+    return  _detectWinner(field1, field2, field3, "X") || _detectWinner(field1, field2, field3, "O");
+} 
+
+function _detectWinner(field1: number, field2: number, field3: number, type: String) {
+    return fields[field1].value == type && fields[field2].value == type && fields[field3].value == type;
+}
+
+function _changePlayer() {
+    if(im == "X") {
+        im = "O";
+        consiglio.innerHTML = "E' il turno di... O";
+    }  else  {
+        im = "X";
+        consiglio.innerHTML = "E' il turno di... X";
+    }
+}
 
 
 
